0dc4a068edffd3764dad7bdfaeedf1b9fb5bd99a
gregorio <mshelzr>
2013-09-11 18:46:47 -0500

    Logueo funcionando

--- beforeIntranete/src/com/intranet/seguridad/action/LoginAction.java
+++ beforeIntranete/src/com/intranet/seguridad/action/LoginAction.java
@@ -1,7 +1,5 @@
 package com.intranet.seguridad.action;
 
-import java.util.Map;
-
 import javax.faces.context.FacesContext;
 
 import org.apache.log4j.Logger;
@@ -10,24 +8,9 @@ import com.intranet.bean.UsuarioDTO;
 import com.intranet.seguridad.dao.UsuarioDAO;
 
 public class LoginAction {
-	
-//	private static final long serialVersionUID = 1L;
-	private UsuarioDTO usuario;
-	private String user;
-	private String pwd;
 
-	public String getUser() {
-		return user;
-	}
-	public void setUser(String user) {
-		this.user = user;
-	}
-	public String getPwd() {
-		return pwd;
-	}
-	public void setPwd(String pwd) {
-		this.pwd = pwd;
-	}
+	private UsuarioDTO usuario;
+
 	public UsuarioDTO getUsuario() {
 		return usuario;
 	}
@@ -35,27 +18,19 @@ public class LoginAction {
 		this.usuario = usuario;
 	}
 	public String execute(){
-		//Modificando s
-//		System.out.println(usuario.getUser());
+		//Modificando
 		UsuarioDAO usuarioDao=new UsuarioDAO();
 //		PerfilDAO perfilDao=new PerfilDAO();
 		
-//		usuario=usuarioDao.validarUsuario(usuario);
-//		usuario=new UsuarioDTO();
-//		usuario.setUser("alu");
-//		FacesContext fc=FacesContext.getCurrentInstance();
-//		UsuarioDTO u=(UsuarioDTO)fc.getExternalContext().getSessionMap().get("usuario");
-//		System.out.println(u.getUser());
-//		PerfilDTO perfil2=perfilDao.buscarPerfil(usuario2);
-//		FacesContext fcx=FacesContext.getCurrentInstance();
-//		fcx.getExternalContext().getSessionMap().put("usuario", usuario);
-//		if(usuario!=null){
-			setUser("Juanon");
-//			Logger.getLogger(LoginAction.class).info("Usuario!=null");
-//			System.out.println(usuario.getUser());
+		UsuarioDTO usuarioValido=usuarioDao.validarUsuario(usuario);
+		FacesContext fc=FacesContext.getCurrentInstance();
+		fc.getExternalContext().getSessionMap().put("usuario",usuario);
+		
+		if(usuarioValido!=null){
+			Logger.getLogger(LoginAction.class).info("Usuario!=null");
 			return "success";
-//		}
-//		return "error";
+		}
+		return "error";
 	}
 
 
